2ad648cd81ea3fa24984ad031955ff05820fef77
taKassi <leowl0279@gmail.com>
2016-02-21 02:48:00 +0900

    名前と住所のスクレイピング
    Kiyomizu::Crawlers::TownPage#scrape

--- lib/kiyomizu/crawlers/townpage.rb
+++ lib/kiyomizu/crawlers/townpage.rb
@@ -7,30 +7,46 @@ module Kiyomizu
 
       # Kiyomizu::Crawlers::Common#create_base_fileをオーバーライド
 
-      def create_base_file
-        puts '#create_base_file使用不可 #scrapeを使用すること'
+      def create_base_file(url)
+        doc = Nokogiri::HTML(open(url))
+
+        open(self.scraped_file_name, "a") do |file|
+          file.puts doc
+        end
       end
 
       # ソースコードの解析
-      def scrape(file_name_in_htmls_derectory)
-        doc = Nokogiri::HTML(open("#{Dir.pwd}/htmls/#{file_name_in_htmls_derectory}"))
+      def scrape(file_name)
+        doc = Nokogiri::HTML(open("#{Dir.pwd}/htmls/#{file_name}"))
 
-        infomation = []
-        doc.css().each do |node|
-          info = []
-
-          # 名前の抽出
-
-          # 住所を抽出
-
-          # TELの抽出
-
-          # FAXの抽出
-
-          # 抽出した要素の配列をはinofmationへpush
-          infomation << info
+        # 名前の抽出し
+        names = doc.css('h4.clearfix').text.gsub("クーポン", "").gsub(" ", "").gsub("\r", "").split("\n")
+        names.map! do |name|
+          unless name == ""
+            name
+          end
         end
-        infomation
+
+        names.compact!
+
+        # 住所を抽出
+        addresses = doc.xpath('//*[@id="wrapAllInside"]/div/div[2]/div/div[2]/div/article/section/p[2]').text
+        addresses = addresses.split('住所')
+        addresses.delete_at(0)
+
+        addresses.map! do |address|
+          match = /(東京都.+)(地図.+)/.match(address)
+          match[1]
+        end
+
+        # TELの抽出
+
+
+        # FAXの抽出
+
+        # 抽出した要素の配列をはinofmationへpush
+        # infomation << info
+        # infomation
       end
     end
   end
