0bb6509464f368e1762607319cbda4c3041d319d
taKassi <leone0279@icloud.com>
2016-02-20 18:05:15 +0900

    あんま重要じゃないけど、ファイル名をDRY

--- lib/kiyomizu/crawlers/common.rb
+++ lib/kiyomizu/crawlers/common.rb
@@ -5,12 +5,14 @@ require "kiyomizu/file_maker"
 module Kiyomizu
   module Crawlers
     class Common
-      attr_reader :name
+      attr_reader :name, :scraped_file_name
 
       include Kiyomizu::FileMaker
 
       def initialize
         @name = self.class.to_s.scan(/\w+$/)[0]
+        @scraped_file_name =
+          "#{Dir.pwd}/htmls/#{self.name}_#{Time.now.strftime('%Y%m%d%H%M%S')}.txt"
       end
       # ごあいさつ
       def greet
@@ -20,8 +22,8 @@ module Kiyomizu
       # SiteからHTMLを抽出してファイルへ
       def create_base_file(url)
         doc = Nokogiri::HTML(open(url))
-        file_name ="#{Dir.pwd}/htmls/#{self.name}_#{Time.now.strftime('%Y%m%d%H%M%S')}.txt"
-        open(file_name, "w") do |file|
+
+        open(self.scraped_file_name, "w") do |file|
           file.puts doc
         end
       end
