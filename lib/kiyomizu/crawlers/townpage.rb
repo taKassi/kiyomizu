23274c6ee69240a237c8511789c75f4008fc2060
taKassi <leone0279@icloud.com>
2016-02-20 18:16:53 +0900

    Kiyomizu::Crawlers::townpage#create_base_fileはつぶす

--- lib/kiyomizu/crawlers/townpage.rb
+++ lib/kiyomizu/crawlers/townpage.rb
@@ -7,21 +7,8 @@ module Kiyomizu
 
       # Kiyomizu::Crawlers::Common#create_base_fileをオーバーライド
 
-      def create_base_file(urls)
-        options = { depth_limint: 0,
-                    skip_query_string: true,
-                    delay: 1 }
-
-        Anemone.crawl(urls, options) do |anemone|
-          anemone.on_every_page do |page|
-            doc = Nokogiri::HTML(page.body)
-
-
-            open(self.scraped_file_name, "a") do |file|
-              file.puts doc
-            end
-          end
-        end
+      def create_base_file
+        puts '#create_base_file使用不可 #scrapeを使用すること'
       end
 
       # ソースコードの解析
